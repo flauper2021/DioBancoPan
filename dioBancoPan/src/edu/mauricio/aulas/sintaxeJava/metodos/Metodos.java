23204e7a1068a7fe1c78bb9b7c56684628813b40
Mauricio Alejandro Juica Arriagada <89223275+flauper2021@users.noreply.github.com>
2023-03-21 09:42:13 -0300

    resumo criação metodo

---
+++ dioBancoPan/src/edu/mauricio/aulas/sintaxeJava/metodos/Metodos.java
@@ -0,0 +1,27 @@
+/* Metodos:
+são funções ou sub-rotinas disponiveis de classes.
+
+Algumas recomendações:
+
+Deve ser nomeado com verbo;
+Deve seguir o padrão camelCase;
+Não se deve criar metodos globais;
+podem ter parametros ou não;
+podem ter retorno ou não;
+o uso da palavra reservada "void" no caso de não haver retorno do metodo;
+
+exemplo:
+
+public class MyClass{
+    public double somar(int parametroUm, int parametroDois){
+        LOGICA - FINALIDADE DO METODO
+        return (retorna um valor)...;
+    }
+
+    public void imprimir(String texto){
+        LOGICA - FINALIDADE DO METODO
+        AQUI NÃO PRECISA DE RETORNO
+    }
+}
+
+ */
